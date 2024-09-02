d3073a88bffa52760efe29571159c2fa23cc8988
Isaiah Lai_ons <Isaiah.Lai@ons.gov.uk>
2024-09-02 16:48:24 +0100

    Use batch delete

--- delete-datasets/src/main.py
+++ delete-datasets/src/main.py
@@ -43,28 +43,63 @@ def mark_dataset_as_deleted(doc_ref: str, firestore_client: firestore.Client):
     document_reference.update({"deleted": True})
 
 
-def delete_document_and_subcollections(doc_ref: firestore.DocumentReference):
+def delete_dataset_with_dataset_id(doc_ref: firestore.DocumentReference, client: firestore.Client) -> None:
+        """
+        Deletes the dataset with the specified dataset id.
+
+        Parameters:
+        dataset_id (str): The unique id of the dataset
+        """
+        try:
+            for sub_collection in doc_ref.collections():
+                delete_sub_collection_in_batches(sub_collection, client)
+
+            doc.reference.delete()
+
+        except Exception as e:
+            raise RuntimeError("Error deleting dataset.")
+
+def delete_sub_collection_in_batches(
+    self,
+    sub_collection_ref: firestore.CollectionReference,
+    client: firestore.Client,
+    batch_size: int = 100
+) -> None:
     """
-    Function that will delete a document and all of its subcollections.
-    
+    Deletes a sub collection in batches.
+
     Parameters:
-    doc_ref: The document reference of the document to be deleted.
+    sub_collection_ref (firestore.CollectionReference): The reference to the sub collection
     """
+    try:
+        docs = sub_collection_ref.limit(batch_size).get()
+        doc_count = 0
+
+        batch = client.batch()
+
+        for doc in docs:
+            doc_count += 1
+            batch.delete(doc.reference)
+
+        batch.commit()
+
+        if doc_count < batch_size:
+            return None
+
+        return delete_sub_collection_in_batches(sub_collection_ref, client)
+
+    except Exception as e:
+        raise RuntimeError("Error deleting sub collection in batches.")
+
+
+"""def delete_document_and_subcollections(doc_ref: firestore.DocumentReference):
     
     for subcollections in doc_ref.collections():
         delete_collection_in_batches(subcollections, batch_size=100)
     
     doc_ref.delete()
 
-def delete_collection_in_batches(collection_ref: firestore.CollectionReference, batch_size: int):
-    """
-    Function that will delete a collection in batches.
-    
-    Parameters:
-    collection_ref: The collection reference of the collection to be deleted.
-    batch_size: The size of the batch to be deleted.
-    """
-
+def delete_collection_in_batches(collection_ref: firestore.CollectionReference, batch_size: int): 
     docs = collection_ref.limit(batch_size).get()
     doc_count = 0
 
@@ -75,7 +110,7 @@ def delete_collection_in_batches(collection_ref: firestore.CollectionReference, 
     if doc_count < batch_size:
         return None
 
-    return delete_collection_in_batches(collection_ref, batch_size)
+    return delete_collection_in_batches(collection_ref, batch_size)"""
 
 if __name__ == "__main__":
     
@@ -86,9 +121,12 @@ if __name__ == "__main__":
     doc_guid = fetch_dataset_guid_from_collection(firestore_client)
     if doc_guid is not None:
         doc = fetch_document_for_deletion(doc_guid, firestore_client)
-        delete_document_and_subcollections(doc)
+        delete_dataset_with_dataset_id(doc, firestore_client)
+
         logging.info(f"Dataset GUID: {doc_guid} has been deleted")
+
         mark_dataset_as_deleted(doc_guid, firestore_client)
+        logging.info(f"Dataset GUID: {doc_guid} has been marked as deleted")
     else:
         logging.info("No datasets to delete")
         print("No datasets to delete")
