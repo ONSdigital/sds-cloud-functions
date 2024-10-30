b6841236559f049c9df7ebae23554ea2831fe1d3
“JacobHarrisonONS” <jacobharrison@outlook.com>
2024-10-30 12:18:54 +0000

    cloudfunction triggered from pubsub and grabs raw data from git using path from message and post to schema

--- schema-upload/src/config.py
+++ schema-upload/src/config.py
@@ -4,5 +4,6 @@ from config_helpers import get_value_from_env
 class Config:
     PROJECT_ID = get_value_from_env("PROJECT_ID", "ons-sds-jb")
     PROCESS_TIMEOUT = int(get_value_from_env("PROCESS_TIMEOUT", "3400"))
+    API_URL = get_value_from_env("API_URL", "")
 
 config = Config()
--- schema-upload/src/main.py
+++ schema-upload/src/main.py
@@ -2,10 +2,35 @@ import base64
 
 from cloudevents.http import CloudEvent
 import functions_framework
+from http_hepler import generate_headers, setup_session
+from config import config
+import requests
+import json
 
 logger = logging.getLogger(__name__)
+
 
 # Triggered from a message on a Cloud Pub/Sub topic.
 @functions_framework.cloud_event
 def subscribe(cloud_event: CloudEvent) -> None:
-    logger.debug(base64.b64decode(cloud_event.data["message"]["data"]).decode())
+    payload = json.loads(base64.b64decode(cloud_event['data']).decode('utf-8'))
+    schema = fetch_raw_schema(payload['path'])
+    post_schema(schema)
+
+
+def fetch_raw_schema(path):
+    response = requests.get(f'"https://raw.githubusercontent.com/ONSdigital/sds-schema-definitions/refs/heads/main"{path}')
+    schema = response.content
+
+    return schema
+
+
+def post_schema(data):
+    session = setup_session()
+    headers = generate_headers()
+
+    session.post(
+        f"{config.API_URL}/v1/schema?survey_id={data['survey_id']}",
+        json=data,
+        headers=headers,
+    )
