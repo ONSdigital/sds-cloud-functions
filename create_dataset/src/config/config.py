0e6e701a772b144b9f7ac8875e3eeecf51037434
“JacobHarrisonONS” <jacobharrison@outlook.com>
2024-11-20 15:10:44 +0000

    add firestore repo/client

--- create_dataset/src/config/config.py
+++ create_dataset/src/config/config.py
@@ -7,17 +7,14 @@ class Config:
     TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
     RESPONSE_TIME_ALERT_THRESHOLD = int(get_value_from_env("RESPONSE_TIME_ALERT_THRESHOLD", "1000"))
 
-    FIRESTORE_DB_NAME = get_value_from_env("FIRESTORE_DB_NAME", "ons-sds-sandbox-01")
+    DATABASE = get_value_from_env("FIRESTORE_DB_NAME", "ons-sds-sandbox-01")
     RETAIN_DATASET_FIRESTORE = get_value_from_env("RETAIN_DATASET_FIRESTORE", "False")
     DATASET_BUCKET_NAME = get_value_from_env("DATASET_BUCKET_NAME", "ons-sds-sandbox-01-dataset")
-    SCHEMA_BUCKET_NAME = get_value_from_env("SCHEMA_BUCKET_NAME", "ons-sds-sandbox-01-schema")
     AUTODELETE_DATASET_BUCKET_FILE = get_value_from_env("AUTODELETE_DATASET_BUCKET_FILE", "True")
-    PUBLISH_SCHEMA_TOPIC_ID = get_value_from_env("PUBLISH_SCHEMA_TOPIC_ID", "ons-sds-sandbox-01-publish-schema")
     PUBLISH_DATASET_TOPIC_ID = get_value_from_env("PUBLISH_DATASET_TOPIC_ID", "ons-sds-sandbox-01-publish-dataset")
     PUBLISH_DATASET_ERROR_TOPIC_ID = get_value_from_env("PUBLISH_DATASET_ERROR_TOPIC_ID", "ons-sds-sandbox-01-publish-dataset-error")
 
     CONF = get_value_from_env("CONF", "ons-sds-sandbox-01-conf")
-    SDS_APPLICATION_VERSION = get_value_from_env("SDS_APPLICATION_VERSION", "v1")
     LOG_EXECUTION_ID = get_value_from_env("LOG_EXECUTION_ID", "False")
     LOG_LEVEL = get_value_from_env("LOG_LEVEL", "INFO")
 
