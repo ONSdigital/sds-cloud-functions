336426ae702677261e36e7e6fa4f0baabf7c1ea8
“JacobHarrisonONS” <jacobharrison@outlook.com>
2024-11-20 09:51:18 +0000

    add requirement.txt

--- dataset-create/src/config/config.py
+++ dataset-create/src/config/config.py
@@ -2,10 +2,9 @@ from config_helpers import get_value_from_env
 
 
 class Config:
-    PROJECT_ID = get_value_from_env("PROJECT_ID", "ons-sds-jb")
-    DATABASE = get_value_from_env("DATABASE", "ons-sds-jb-sds")
+    PROJECT_ID = get_value_from_env("PROJECT_ID", "ons-sds-sandbox-01")
+    DATABASE = get_value_from_env("DATABASE", "ons-sds-sandbox-01-sds")
     PROCESS_TIMEOUT = int(get_value_from_env("PROCESS_TIMEOUT", "3400"))
-    DELETION_BATCH_SIZE = int(get_value_from_env("DELETION_BATCH_SIZE", "100"))
     TIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 config = Config()
