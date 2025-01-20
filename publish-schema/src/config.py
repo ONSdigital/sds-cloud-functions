306b2e8a0d84ce5abe0af4b1792fafdd4157f475
jamesbryer <james.bryer@ext.ons.gov.uk>
2025-01-20 15:43:07 +0000

    Add default

--- publish-schema/src/config.py
+++ publish-schema/src/config.py
@@ -4,7 +4,7 @@ from config_helpers import get_value_from_env
 class Config:
     PROJECT_ID = get_value_from_env("PROJECT_ID", "ons-sds-jb")
     PROCESS_TIMEOUT = int(get_value_from_env("PROCESS_TIMEOUT", "540"))
-    SDS_URL = get_value_from_env("SDS_URL", "")
+    SDS_URL = get_value_from_env("SDS_URL", "test_url")
     SECRET_ID = get_value_from_env("SECRET_ID", "oauth-client-id")
     GITHUB_URL = get_value_from_env("GITHUB_URL", "https://raw.githubusercontent.com/ONSdigital/sds-prototype-schema/refs/heads/main/")
     POST_SCHEMA_ENDPOINT = get_value_from_env("POST_SCHEMA_URL", "/v1/schema?survey_id=")
