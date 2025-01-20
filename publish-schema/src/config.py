16cb6d7c789937b4d88a978af02b657434e64a2c
jamesbryer <james.bryer@ext.ons.gov.uk>
2025-01-20 16:04:32 +0000

    Fix var name.

--- publish-schema/src/config.py
+++ publish-schema/src/config.py
@@ -6,7 +6,7 @@ class Config:
     PROCESS_TIMEOUT = int(get_value_from_env("PROCESS_TIMEOUT", "540"))
     SDS_URL = get_value_from_env("SDS_URL", "test_url")
     SECRET_ID = get_value_from_env("SECRET_ID", "oauth-client-id")
-    GITHUB_URL = get_value_from_env("GITHUB_URL", "https://raw.githubusercontent.com/ONSdigital/sds-prototype-schema/refs/heads/main/")
+    GITHUB_SCHEMA_URL = get_value_from_env("GITHUB_URL", "https://raw.githubusercontent.com/ONSdigital/sds-prototype-schema/refs/heads/main/")
     POST_SCHEMA_ENDPOINT = get_value_from_env("POST_SCHEMA_URL", "/v1/schema?survey_id=")
     GET_SCHEMA_METADATA_ENDPOINT = get_value_from_env("GET_SCHEMA_METADATA_URL", "/v1/schema_metadata?survey_id=")
 
