ff2bffdaf9b6a45db4a7e476b8e10cefcaafe5e0
jamesbryer <james.bryer@ext.ons.gov.uk>
2025-02-11 17:32:59 +0000

    Add different conf for int tests

--- publish-schema/src/config/schema_config.py
+++ publish-schema/src/config/schema_config.py
@@ -63,7 +63,7 @@ class SchemaConfig:
     SECRET_ID = ConfigHelpers.get_value_from_env("SECRET_ID", "oauth-client-id")
     GITHUB_SCHEMA_URL = ConfigHelpers.get_value_from_env(
         "GITHUB_SCHEMA_URL",
-        "https://raw.githubusercontent.com/ONSdigital/sds-prototype-schema/refs/heads/main/",
+        "https://raw.githubusercontent.com/ONSdigital/sds-prototype-schema/refs/heads/SDSS-876-schema-automation-implementation/",
     )
     POST_SCHEMA_ENDPOINT = ConfigHelpers.get_value_from_env(
         "POST_SCHEMA_URL", "/v1/schema?survey_id="
@@ -87,32 +87,5 @@ class SchemaConfig:
         "SCHEMA_BUCKET_NAME", "ons-sds-jb-sds-europe-west2-schema"
     )
 
-class IntegrationTestConfig:
-    PROJECT_ID = ConfigHelpers.get_value_from_env("PROJECT_ID", "ons-sds-jb")
-    PROCESS_TIMEOUT = int(ConfigHelpers.get_value_from_env("PROCESS_TIMEOUT", "540"))
-    SDS_URL = ConfigHelpers.get_value_from_env("SDS_URL", "test_url")
-    SECRET_ID = ConfigHelpers.get_value_from_env("SECRET_ID", "oauth-client-id")
-    GITHUB_SCHEMA_URL = "https://raw.githubusercontent.com/ONSdigital/sds-prototype-schema/refs/heads/SDSS-876-schema-automation-implementation/"
-    POST_SCHEMA_ENDPOINT = ConfigHelpers.get_value_from_env(
-        "POST_SCHEMA_URL", "/v1/schema?survey_id="
-    )
-    GET_SCHEMA_METADATA_ENDPOINT = ConfigHelpers.get_value_from_env(
-        "GET_SCHEMA_METADATA_URL", "/v1/schema_metadata?survey_id="
-    )
-    PUBLISH_SCHEMA_ERROR_TOPIC_ID = ConfigHelpers.get_value_from_env(
-        "PUBLISH_SCHEMA_ERROR_TOPIC_ID", "publish-schema-error"
-    )
-    PUBLISH_SCHEMA_SUCCESS_TOPIC_ID = ConfigHelpers.get_value_from_env(
-        "PUBLISH_SCHEMA_SUCCESS_TOPIC_ID", "publish-schema-success"
-    )
-    PUBLISH_SCHEMA_QUEUE_TOPIC_ID = ConfigHelpers.get_value_from_env(
-        "PUBLISH_SCHEMA_QUEUE_TOPIC_ID", "publish-schema-queue"
-    )
-    FIRESTORE_DB_NAME = ConfigHelpers.get_value_from_env(
-        "FIRESTORE_DB_NAME", "ons-sds-jb-sds"
-    )
-    SCHEMA_BUCKET_NAME = ConfigHelpers.get_value_from_env(
-        "SCHEMA_BUCKET_NAME", "ons-sds-jb-sds-europe-west2-schema"
-    )
 
-CONFIG = IntegrationTestConfig() if ConfigHelpers.get_value_from_env("CONF") == "integration-test" else SchemaConfig()
+CONFIG = SchemaConfig()
