13ccd6dddbc5de0d817db54f368c9cc9836fd740
jamesbryer <james.bryer@ons.gov.uk>
2025-11-06 17:16:56 +0000

    tweaks

--- publish-schema-gcs/src/main.py
+++ publish-schema-gcs/src/main.py
@@ -1,6 +1,8 @@
 import functions_framework
 from cloudevents.http import CloudEvent
-
+from sds_common.repositories.bucket_loader import bucket_loader
+from sds_common.repositories.bucket_repository import BucketRepository
+from sds_common.enums.buckets import Bucket
 
 @functions_framework.cloud_event
 def publish_schema_gcs(cloud_event: CloudEvent) -> None:
@@ -10,8 +12,11 @@ def publish_schema_gcs(cloud_event: CloudEvent) -> None:
     Parameters:
         cloud_event: The CloudEvent triggering the function.
     """
-
     bucket_name = cloud_event.data["bucket"]
-    file_name = cloud_event.data["name"]
+    if bucket_name == Bucket.SCHEMA_PUBLISH_BUCKET.value:
+        bucket_repository = BucketRepository(bucket_loader.fetch_bucket(Bucket.SCHEMA_PUBLISH_BUCKET.name))
+        file_name = cloud_event.data["name"]
+        schema_json = bucket_repository.get_file_as_json(file_name)
+        return schema_json
 
     return None
