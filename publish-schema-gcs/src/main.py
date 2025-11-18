e59eeca25d03c0eda599295efbd090793dcc6b7e
jamesbryer <james.bryer@ons.gov.uk>
2025-11-18 14:01:37 +0000

    Many nice things

--- publish-schema-gcs/src/main.py
+++ publish-schema-gcs/src/main.py
@@ -1,11 +1,11 @@
 import functions_framework
 from cloudevents.http import CloudEvent
-from sds_common.repositories.bucket_loader import bucket_loader
-from sds_common.repositories.bucket_repository import BucketRepository
-from sds_common.enums.buckets import Bucket
-from sds_common.schema.schema import Schema
-from sds_common.services.sds_schema_request_service import SDS_SCHEMA_REQUEST_SERVICE
 from sds_common.config.logging_config import logging
+from sds_common.enums.buckets import Bucket
+from sds_common.repositories.bucket_loader import BucketLoader
+from sds_common.schema.schema import Schema
+from sds_common.services.bucket_service import BucketService
+from sds_common.services.sds_schema_request_service import SDS_SCHEMA_REQUEST_SERVICE
 
 logger = logging.getLogger(__name__)
 
@@ -21,13 +21,18 @@ def publish_schema_gcs(cloud_event: CloudEvent) -> None:
     bucket_name = cloud_event.data["bucket"]
     file_name = cloud_event.data["name"]
 
-    if bucket_name == Bucket.SCHEMA_PUBLISH_BUCKET.value:
-        bucket_repository = BucketRepository(bucket_loader.fetch_bucket(Bucket.SCHEMA_PUBLISH_BUCKET))
-        logger.info("Fetching schema file: %s", file_name)
-        schema_json = bucket_repository.get_file_as_json(file_name)
-        schema = Schema.set_schema(schema_json)
-        SDS_SCHEMA_REQUEST_SERVICE.post_schema(schema)
-    else:
-        logger.info(f"Bucket name is: {bucket_name} not schema publish bucket: {Bucket.SCHEMA_PUBLISH_BUCKET}, exiting function.")
+    if not bucket_name == Bucket.SCHEMA_PUBLISH_BUCKET.value:
+        logger.info(
+            f"Bucket name is: {bucket_name} not schema publish bucket: {Bucket.SCHEMA_PUBLISH_BUCKET}, exiting function.")
+        return None
+
+    bucket_service = BucketService(Bucket.SCHEMA_PUBLISH_BUCKET, BucketLoader())
+    schema_json = bucket_service.retrieve_json_file_from_bucket(file_name)
+    schema = Schema.set_schema(schema_json)
+    response = SDS_SCHEMA_REQUEST_SERVICE.post_schema(schema)
+
+    if response.status_code == 200:
+        logger.debug(f"Schema {file_name} published successfully, deleting schema file from bucket.")
+        bucket_service.delete_file_from_bucket(file_name)
 
     return None
