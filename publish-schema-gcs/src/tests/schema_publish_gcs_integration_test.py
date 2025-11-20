7fb8e3e267aec5e6cd703ff788a1181297dd6fd9
jamesbryer <james.bryer@ons.gov.uk>
2025-11-20 11:25:37 +0000

    Tweaks

--- publish-schema-gcs/src/tests/schema_publish_gcs_integration_test.py
+++ publish-schema-gcs/src/tests/schema_publish_gcs_integration_test.py
@@ -55,7 +55,7 @@ class SchemaPublishGcsIntegrationTest(TestCase):
         *We assert that the schema was published successfully.
 
         """
-        self.bucket_service.upload_file_to_bucket("tests/test_data/test_schema_success.json")
+        self.bucket_service.upload_file_to_bucket("src/tests/test_data/test_schema_success.json")
 
         messages = poll_subscription(
             self.schema_success_pubsub_helper, test_schema_subscriber_id_success
