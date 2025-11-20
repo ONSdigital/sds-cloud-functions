0281e5b43442ad37ab1a426a6d4c364e085e7be3
jamesbryer <james.bryer@ons.gov.uk>
2025-11-20 14:27:32 +0000

    Cleanup for sds-common v0.0.34

--- publish-schema-gcs/src/main.py
+++ publish-schema-gcs/src/main.py
@@ -1,8 +1,8 @@
 import functions_framework
 from cloudevents.http import CloudEvent
 from sds_common.config.logging_config import logging
+from sds_common.models.schema_publish_errors import SchemaPublishError
 from sds_common.publishers.gcs_schema_publisher import GcsSchemaPublisher
-from sds_common.services.sds_schema_request_service import SdsSchemaRequestService
 
 logger = logging.getLogger(__name__)
 
@@ -17,11 +17,13 @@ def publish_schema_gcs(cloud_event: CloudEvent) -> None:
     """
     file_name = cloud_event.data["name"]
 
-    publisher = GcsSchemaPublisher()
-    response = publisher.publish(file_name)
-
-    if response.status_code == 200:
-        logger.debug(f"Deleting schema file {file_name} from bucket.")
-        publisher.cleanup(file_name)
+    try:
+        publisher = GcsSchemaPublisher()
+        response = publisher.publish(file_name)
+        if response.status_code == 200:
+            logger.debug(f"Deleting schema file {file_name} from bucket.")
+            publisher.cleanup(file_name)
+    except SchemaPublishError as e:
+        logger.error(e.error_message)
 
     return None
