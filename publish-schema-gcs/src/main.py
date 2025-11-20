226bd159c44b237fa1575577f8b67ab0a8bca6a2
jamesbryer <james.bryer@ons.gov.uk>
2025-11-20 14:25:17 +0000

    Cleanup for sds-common v0.0.34

--- publish-schema-gcs/src/main.py
+++ publish-schema-gcs/src/main.py
@@ -1,11 +1,8 @@
 import functions_framework
 from cloudevents.http import CloudEvent
 from sds_common.config.logging_config import logging
-from sds_common.enums.buckets import Bucket
-from sds_common.repositories.bucket_loader import BucketLoader
-from sds_common.schema.schema import Schema
-from sds_common.services.bucket_service import BucketService
-from sds_common.services.sds_schema_request_service import SDS_SCHEMA_REQUEST_SERVICE
+from sds_common.publishers.gcs_schema_publisher import GcsSchemaPublisher
+from sds_common.services.sds_schema_request_service import SdsSchemaRequestService
 
 logger = logging.getLogger(__name__)
 
@@ -18,22 +15,13 @@ def publish_schema_gcs(cloud_event: CloudEvent) -> None:
     Parameters:
         cloud_event: The CloudEvent triggering the function.
     """
-    bucket_name = cloud_event.data["bucket"]
     file_name = cloud_event.data["name"]
 
-    if not bucket_name == Bucket.SCHEMA_PUBLISH_BUCKET.value:
-        logger.info(
-            f"Bucket name is: {bucket_name} not schema publish bucket. Exiting function."
-        )
-        return None
-
-    bucket_service = BucketService(Bucket.SCHEMA_PUBLISH_BUCKET, BucketLoader())
-    schema_json = bucket_service.retrieve_json_file_from_bucket(file_name)
-    schema = Schema.set_schema(schema_json, file_name)
-    response = SDS_SCHEMA_REQUEST_SERVICE.post_schema(schema)
+    publisher = GcsSchemaPublisher()
+    response = publisher.publish(file_name)
 
     if response.status_code == 200:
         logger.debug(f"Deleting schema file {file_name} from bucket.")
-        bucket_service.delete_file_from_bucket(file_name)
+        publisher.cleanup(file_name)
 
     return None
--- publish-schema/src/main.py
+++ publish-schema/src/main.py
@@ -5,11 +5,8 @@ from cloudevents.http import CloudEvent
 from sds_common.config.logging_config import logging
 from sds_common.config.config import CONFIG
 from sds_common.models.schema_publish_errors import SchemaPublishError
-from sds_common.schema.schema import Schema
+from sds_common.publishers.pubsub_schema_publisher import PubsubSchemaPublisher
 from sds_common.services.pub_sub_service import PUB_SUB_SERVICE
-from sds_common.services.sds_schema_request_service import SDS_SCHEMA_REQUEST_SERVICE
-from sds_common.services.schema_validator_service import SCHEMA_VALIDATOR_SERVICE
-from sds_common.utilities.utils import fetch_raw_schema_from_github
 
 logger = logging.getLogger(__name__)
 
@@ -25,13 +22,8 @@ def publish_schema(cloud_event: CloudEvent) -> None:
     filepath = base64.b64decode(cloud_event.data["message"]["data"]).decode("utf-8")
 
     try:
-        schema_json = fetch_raw_schema_from_github(filepath)
-
-        schema = Schema.set_schema(schema_json, filepath)
-
-        SCHEMA_VALIDATOR_SERVICE.validate_schema(schema)
-
-        SDS_SCHEMA_REQUEST_SERVICE.post_schema(schema)
+        publisher = PubsubSchemaPublisher()
+        publisher.publish(filepath)
     except SchemaPublishError as e:
         logger.error(e.error_message)
         PUB_SUB_SERVICE.send_message(e, CONFIG.PUBLISH_SCHEMA_ERROR_TOPIC_ID)
